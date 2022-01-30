463b729417407a992d85f5b97b06b82caf7fd607
palka <7paulina.sadowska@gmail.com>
2022-01-30 19:37:54 +0100

    conflicts

--- src/main/kotlin/com/github/paulinasadowska/detektcustomruleplayground/MyRuleSetProvider.kt
+++ src/main/kotlin/com/github/paulinasadowska/detektcustomruleplayground/MyRuleSetProvider.kt
@@ -3,6 +3,7 @@ package com.github.paulinasadowska.detektcustomruleplayground
 import io.gitlab.arturbosch.detekt.api.Config
 import io.gitlab.arturbosch.detekt.api.RuleSet
 import io.gitlab.arturbosch.detekt.api.RuleSetProvider
+import org.example.detekt.VersionRule
 
 class MyRuleSetProvider : RuleSetProvider {
     override val ruleSetId: String = "MyRuleSet"
--- src/main/kotlin/com/github/paulinasadowska/detektcustomruleplayground/RequireVersionExample.kt
+++ src/main/kotlin/com/github/paulinasadowska/detektcustomruleplayground/RequireVersionExample.kt
@@ -16,9 +16,3 @@ class Bar {
     @RequiresVersion("1.2.0")
     fun a() = "I'm not ready yet!"
 }
-
-
-@RequiresOptIn(message = "This API is experimental. It may be changed in the future without notice.")
-@Retention(AnnotationRetention.BINARY)
-@Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION)
-annotation class MyDateTime // Opt-in requirement annotation
